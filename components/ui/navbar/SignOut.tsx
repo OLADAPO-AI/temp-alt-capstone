fc50e0f8fafbaa2e3906b83bb4b7efd2ff5b681b
Oladapo Falusi <oladapofalusi@gmail.com>
2024-08-15 21:43:36 +0100

    AUTH PROVIDEDER ADDED

--- components/ui/navbar/SignOut.tsx
+++ components/ui/navbar/SignOut.tsx
@@ -1,5 +1,19 @@
+'use client'
+import { SignOutButton } from '@clerk/nextjs'
+import { useToast } from '@/components/ui/use-toast'
+import Link from 'next/link'
 function SignOut() {
-  return <div>SignOut</div>
+  const { toast } = useToast()
+  const handleLogout = () => {
+    toast({ description: 'Logging Out...' })
+  }
+  return (
+    <SignOutButton>
+      <Link href="/" className="w-full text-left" onClick={handleLogout}>
+        Logout
+      </Link>
+    </SignOutButton>
+  )
 }
 
 export default SignOut
