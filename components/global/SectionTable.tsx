ce7e69987ff357ca904e6ae1f585459ef4e0642a
Oladapo Falusi <oladapofalusi@gmail.com>
2024-08-16 15:55:25 +0100

    Test a Component

--- components/global/SectionTable.tsx
+++ components/global/SectionTable.tsx
@@ -78,7 +78,7 @@ const SectionTable = ({ hospitals }: { hospitals: Hospital[] }) => {
                   Level
                 </th>
                 <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">
-                  Ownership
+                  Ownershipp
                 </th>
                 <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">
                   Actions
